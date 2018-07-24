0bcb5540b6d53853ae469b0ba2fff50ba5041071
zhibin.shao <275803332@163.com>
2018-07-24 14:21:11 +0800

    1

--- src/main/java/com/t7241/web/WebApplication.java
+++ src/main/java/com/t7241/web/WebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
-@ComponentScan(basePackages = "com.t7241.web")
+//@ComponentScan(basePackages = "com.t7241.web")
 public class WebApplication {
 
     public static void main(String[] args) {
